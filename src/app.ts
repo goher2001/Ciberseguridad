720456ae685e08fe0a6dd76ec5b912ba1d2bde0c
goher2001 <christiangohet@gmail.com>
2022-12-08 10:34:26 +0100

    Commit

--- src/app.ts
+++ src/app.ts
@@ -7,27 +7,8 @@ import * as bcu from 'bigint-crypto-utils'
 
 //Constants
 const port = 3000;
-let keyRSA: ClaveRSA.rsaKeyPair;
-let randomGlobal: bigint;
-let user1: user.User = {
-  name: "Christian",
-  password: '1234',
-  pubKey: 147109628126100023593239446490899395001078431760853540398962704792489738934991397248021316777065115915633380757011226090175671693614646227957074381068067568099358213740198667523758748645509515291106503899283478504350755335804498133153167529339067898543596931834333269810934666919976567062886159234861679651851n,
-  creditos: 30
-};
-let user2: user.User = {
-  name: "Jaskie",
-  password: '1234',
-  pubKey: 179594136643155679113162790150632306207235392284218462592197681766549555344457458652339139580350769900680575559082556317850630407510138639768279602686282320375294868038048143428864756116717586319849244422467220085662067639465680690200899115829038312033310126898261753276541121228030800306368072902199867842091n,
-  creditos: 15
-};
-let user3: user.User = {
-  name: "Ferran",
-  password: '1234',
-  pubKey: 0n,
-  creditos: 30
-};
-let classes: Array<user.User> = [user1,user2,user3];
+
+
 
 //Inizializations
 const app = express()
@@ -46,22 +27,18 @@ app.listen(port, function () {
 
 app.post('/registrar', async (req, res) => {
   try{
-    console.log(req.body.username)
-    console.log(req.body.password);
+ 
   var keys = await ClaveRSA.generateKeys(1024);
-  var username:string = req.body.user;
-  var password:string = req.body.password;
-    //var found = classes.find()
-  var newuser: user.User = new user.User(username,password,keys.publicKey.e,100)
-  console.log(keys.publicKey.n)
 
-  const json = '{"privateKey": "' + keys.privateKey.d + '"}';
-  console.log()
+  const json = '{"privD": "' + keys.privateKey.d + '","privN": "'+keys.privateKey.n+'","pubE":"'+keys.publicKey.e+'"}';
+  
   return res.status(200).json(JSON.parse(json));
 } catch (err) {
   console.log(err);
   return res.status(400).json(err);
 }})
+
+
 
 app.get('/verificar/:mensaje/:username', async (req, res) => {
   
